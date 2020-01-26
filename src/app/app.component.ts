fddbec640e3856b1316d3dd2cee236ba9b55dbbd
Lukasz Wojcik <lukasz.wojcik.96@gmail.com>
2020-01-26 17:41:07 +0100

    add dark theme + working image uploader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgxFileDropEntry, FileSystemFileEntry, FileSystemDirectoryEntry } from 'ngx-file-drop';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'image-manipulator-client';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,52 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from "@angular/common/http";
+import {ImageManipulatorComponent} from './image-manipulator/image-manipulator.component';
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {MatInputModule} from "@angular/material/input";
+import {MatListModule} from "@angular/material/list";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {NgxFileDropModule} from "ngx-file-drop";
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FileUploadComponent } from './file-upload/file-upload.component';
+import {MatIconModule} from "@angular/material/icon";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatSliderModule} from "@angular/material/slider";
+import {PinchZoomModule} from "ngx-pinch-zoom";
+import {FileUploadModule} from "ng2-file-upload";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageManipulatorComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    FileUploadComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    MatToolbarModule,
+    NgxFileDropModule,
+    MatIconModule,
+    MatGridListModule,
+    MatSliderModule,
+    PinchZoomModule,
+    FileUploadModule
   ],
   providers: [],
   bootstrap: [AppComponent]
